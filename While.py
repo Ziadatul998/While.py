35b62fb4051b2060c09ca1c39aa4feb09dffa4b6
Ziadatul998 <lunaziadatulawlyaalhusna@gmail.com>
2024-10-22 07:09:02 +0800

    Create While.py

---
+++ While.py
@@ -0,0 +1,10 @@
+numbers = []
+num = 0
+
+while num >= 0:
+    num = int(input("masukan angka(negatif untuk berhenti) : "))
+    if num >= 0:
+        numbers.append(num)
+        
+print("angka yang dikumpulkan :",numbers)
+        
